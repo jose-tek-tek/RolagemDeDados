8ca10bde15fc484d365023dd8497fc55e3bf5e72
Pedrox | Adm! Dev! <163602203+jose-tek-tek@users.noreply.github.com>
2024-03-31 18:57:38 -0300

    Rolagem de Dados

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿﻿using System.Security.Cryptography;
+
+Console.WriteLine("Rolagem de dados");
+
+Console.WriteLine("Quantas faces tem o dado?");
+
+int faces = Convert.ToInt32(Console.ReadLine());
+
+int rolagem = RandomNumberGenerator.GetInt32(1, faces);
+
+Console.WriteLine("");
+
+Console.WriteLine($"A rolagem do dado resultou em {rolagem}");
